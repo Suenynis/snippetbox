808af7367c6c19b87e4c6ecc277d19ecf1558fc2
baltabaeva <baltabaeva005@mail.ru>
2022-11-12 16:22:08 +0600

    final project

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -27,7 +27,7 @@ func (app *application) routes() http.Handler {
 	router.Handler(http.MethodGet, "/snippet/create", protected.ThenFunc(app.snippetCreate))
 	router.Handler(http.MethodPost, "/snippet/create", protected.ThenFunc(app.snippetCreatePost))
 	router.Handler(http.MethodPost, "/user/logout", protected.ThenFunc(app.userLogoutPost))
-	router.Handler(http.MethodPost, "/snippet/delete/:id", protected.ThenFunc(app.Deletepost))
+	router.Handler(http.MethodGet, "/snippet/delete/:id", dynamic.ThenFunc(app.Deletepost))
 	standard := alice.New(app.recoverPanic, app.logRequest, secureHeaders)
 	return standard.Then(router)
 }
